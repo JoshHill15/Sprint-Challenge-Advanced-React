51753a690849004f8e9de08688fd170be62e9bdf
josh Hill <Josh_hill15@me.com>
2019-12-06 12:06:08 -0500

    mvp

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render, fireEvent, toBeInTheDocument} from "@testing-library/react"
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+test("renders app", () => {
+  render(<App />)
+})
+
+
+test('it renders Women', () => {
+  const { getByText } = render(<App />);
+  const elem = getByText(/Women/);
+  expect(elem).toBeInTheDocument();
 });
+
